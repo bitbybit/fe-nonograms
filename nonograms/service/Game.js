a3c95327d8240a9a7d16693f9b8c2c28c3df0984
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-02-01 20:14:47 +0100

    feat: container styles

--- nonograms/service/Game.js
+++ nonograms/service/Game.js
@@ -80,36 +80,60 @@ export class Game {
   #initContainer() {
     const container = new Component({
       name: 'container',
-      classList: ['container']
+      classList: ['container', 'd-flex', 'flex-column']
     })
 
     const rows = new Component({
       name: 'rows',
-      classList: ['row'],
+      classList: ['row', 'flex-grow-1'],
       $container: container.$element
     })
 
     this.#selectorWrapper = new Component({
       name: 'selector-wrapper',
-      classList: ['col-4', 'p-2', 'text-center'],
+      classList: [
+        'col-6',
+        'p-2',
+        'align-content-center',
+        'bg-secondary-subtle'
+      ],
       $container: rows.$element
     })
 
     this.#resetWrapper = new Component({
       name: 'reset-wrapper',
-      classList: ['col-4', 'p-2', 'text-center'],
+      classList: [
+        'col-3',
+        'p-2',
+        'align-content-center',
+        'bg-secondary-subtle'
+      ],
       $container: rows.$element
     })
 
     this.#stopwatchWrapper = new Component({
       name: 'stopwatch-wrapper',
-      classList: ['col-4', 'p-2', 'text-center'],
+      classList: [
+        'col-3',
+        'p-2',
+        'align-content-center',
+        'text-center',
+        'bg-secondary-subtle',
+        'text-primary'
+      ],
       $container: rows.$element
     })
 
     this.#canvasWrapper = new Component({
       name: 'canvas-wrapper',
-      classList: ['col-12', 'p-2', 'text-center'],
+      classList: [
+        'col-12',
+        'p-2',
+        'd-flex',
+        'flex-grow-1',
+        'align-items-center',
+        'justify-content-center'
+      ],
       $container: rows.$element
     })
   }
@@ -129,6 +153,8 @@ export class Game {
         this.#state.level = detail.level
         this.#state.template = detail.template
 
+        this.#resetStopwatch()
+
         this.#canvas.clearCells()
         this.#drawCanvas()
       }
@@ -141,9 +167,10 @@ export class Game {
     })
 
     reset.events.addEventListener('click', () => {
-      this.#stopwatch.reset()
-      this.#state.initDefaultCells()
+      this.#resetStopwatch()
+
       this.#canvas.clearCells()
+      this.#state.initDefaultCells()
     })
   }
 
@@ -175,10 +202,9 @@ export class Game {
        * @param {CanvasPayload} payload
        */
       ({ detail }) => {
-        this.#stopwatch.start()
+        this.#startStopwatch()
 
         const { x, y, value } = detail
-
         this.#state.cells[y][x] = value
       }
     )
@@ -191,5 +217,19 @@ export class Game {
       verCellAmount: this.#state.template.verCellAmount,
       verHints: this.#state.template.verHints
     })
+  }
+
+  #startStopwatch() {
+    this.#stopwatchWrapper.$element.classList.remove('bg-secondary-subtle')
+    this.#stopwatchWrapper.$element.classList.add('bg-dark-subtle')
+
+    this.#stopwatch.start()
+  }
+
+  #resetStopwatch() {
+    this.#stopwatchWrapper.$element.classList.remove('bg-dark-subtle')
+    this.#stopwatchWrapper.$element.classList.add('bg-secondary-subtle')
+
+    this.#stopwatch.reset()
   }
 }
